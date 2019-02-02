db5a5c7eea4ea5a554a13c14bba348fab95e5130
Sean Houde <seanhoude@gmail.com>
2019-02-02 12:47:43 -0500

    add slides

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
-const $nameEl = $("#name-el")
+const $projects = $("#projects")
 const $carouselOne = $("#carousel-one")
 
 $(document).ready(function() {
-  $nameEl.addClass("animated fadeIn")
-  $carouselOne.addClass("animated slideInRight")
+  // $projects.addClass("animated slideInRight")
 })
 
 $('[data-mobile-app-filter-menu] li').click(function () {
-  $(this).siblings().removeClass('is-active');
+  $(this).siblings().removeClass("is-active");
   $(this).addClass('is-active');
-  let firstToHide = "#" + $(this).siblings().first().text().toLowerCase()
-  let lastToHide = "#" + $(this).siblings().last().text().toLowerCase()
-  let idToShow = "#" + $(this).children("a").text().toLowerCase()
-  $(firstToHide).hide()
-  $(lastToHide).hide()
-  $(idToShow).show()
+  let firstToHide = $(this).siblings().first().text().toLowerCase()
+  let lastToHide = $(this).siblings().last().text().toLowerCase()
+  let idToShow = $(this).children("a").text().toLowerCase()
+
+  let $firstToHide = $("#" + firstToHide)
+  let $lastToHide = $("#" + lastToHide)
+  let $idToShow = $("#" + idToShow)
+  $firstToHide.removeClass().addClass("animated slideOutLeft")
+  $lastToHide.removeClass().addClass("animated slideOutLeft")
+  $idToShow.removeClass()
+  $idToShow.show()
+  $idToShow.addClass("is-active")
+  // document.getElementById(firstToHide).addEventListener('animationend', function() { $firstToHide.hide() })
+  // document.getElementById(lastToHide).addEventListener('animationend', function() { $lastToHide.hide() })
+  $idToShow.addClass("animated slideInRight")
 });
 
