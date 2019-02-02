73f9204616276887667070d31b307d6964210e3e
Sean Houde <seanhoude@gmail.com>
2019-02-02 11:00:26 -0500

    add nav tabs with js functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+const $nameEl = $("#name-el")
+const $carouselOne = $("#carousel-one")
+
 $(document).ready(function() {
-  $("#name-el").addClass("animated", "bounce", "delay-2s")
+  $nameEl.addClass("animated fadeIn")
+  $carouselOne.addClass("animated slideInRight")
 })
+
+$('[data-mobile-app-filter-menu] li').click(function () {
+  $(this).siblings().removeClass('is-active');
+  $(this).addClass('is-active');
+  let firstToHide = "#" + $(this).siblings().first().text().toLowerCase()
+  let lastToHide = "#" + $(this).siblings().last().text().toLowerCase()
+  let idToShow = "#" + $(this).children("a").text().toLowerCase()
+  $(firstToHide).hide()
+  $(lastToHide).hide()
+  $(idToShow).show()
+});
+
